8992e9029fc54a3a4d885afc304406712030304d
byndasma <byndasma@gmail.com>
2018-08-24 00:13:22 -0400

    handling user input

--- first-project/WeatherApp.js
+++ first-project/WeatherApp.js
@@ -1,19 +1,27 @@
 import WeatherProject from '..WeatherApp';
-export default WeatherApp;
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default class App extends React.Component {
+class WeatherApp extends Component {
   constructor(props) {
     super(props);
-    this.state = { zip: ''};
+    this.state = {zip: ''};
+    _handleTextChange = event => {
+      this.setState({ zip: event.nativeEvent.text });
+    }
   }
   render() {
     return (
-      <Text style={styles.welcome}>
-        You input {this.state.zip}.
-      </Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          You input {this.state.zip}.
+        </Text>
+        <TextInput 
+          style={styles.input}
+          onSubmitEditing={this._handleTextChange}
+        />
+      </View>
     );
   }
 }
@@ -25,9 +33,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
   input: {
     fontSize: 20,
     borderWidth: 2,
     height: 40,
   },
 });
+
+export default WeatherApp;
